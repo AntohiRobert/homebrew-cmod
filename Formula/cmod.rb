220081c9b8245c9ca66b6148719aef7ec13157c9
Antohi Robert <antohir@gmail.com>
2026-04-04 00:45:43 +0300

    fix force brewgcc on macos...again

--- Formula/cmod.rb
+++ Formula/cmod.rb
@@ -4,41 +4,25 @@ class Cmod < Formula
   url "https://github.com/AntohiRobert/cmod/archive/refs/heads/main.tar.gz"
   version "0.1.0"
 
+
   depends_on "python@3.12"
+
   depends_on "git"
+
   depends_on "gcc@11"
 
   def install
-    # 1. Instalăm scriptul python original într-o locație privată (libexec)
-    libexec.install "cmod.py"
+  chmod 0755, "cmod.py"
 
-    # 2. Creăm un wrapper în bin/cmod care setează PATH-ul înainte de execuție
-    # Acest wrapper va pune folderul bin de la GCC 11 în fața /usr/bin
-    # și va crea un alias temporar (symlink) de la g++-11 la g++
-    
-    gcc_bin = Formula["gcc@11"].opt_bin
-    
-    (bin/"cmod").write <<~EOS
-      #!/bin/bash
-      # Creăm un folder temporar pentru a face maparea g++ -> g++-11
-      temp_bin=$(mktemp -d)
-      ln -s "#{gcc_bin}/g++-11" "$temp_bin/g++"
-      ln -s "#{gcc_bin}/gcc-11" "$temp_bin/gcc"
-      
-      # Executăm scriptul python cu noul PATH setat
-      PATH="$temp_bin:#{gcc_bin}:$PATH" #{Formula["python@3.12"].opt_bin}/python3.12 "#{libexec}/cmod.py" "$@"
-      
-      # Curățăm folderul temporar după execuție
-      rm -rf "$temp_bin"
-    EOS
+  gcc=Formula["gcc@11"]
+  ENV["CC"] = Formula["gcc@11"].opt_bin/"gcc-11"
+  ENV["CXX"] = Formula["gcc@11"].opt_bin/"g++-11"
 
-    chmod 0755, bin/"cmod"
+  bin.install "cmod.py" => "cmod"
   end
 
   test do
     system "#{bin}/cmod", "init"
-    # Verificăm că a rămas "g++" curat în config
-    assert_match '"command": "g++ -fmodules-ts"', File.read("cmodconfig.json")
     assert_predicate testpath/"cmodconfig.json", :exist?
   end
 end
