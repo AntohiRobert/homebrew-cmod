de8ad3f7575bf154cb7e6741615bba82b048d81f
Antohi Robert <antohir@gmail.com>
2026-04-04 01:21:05 +0300

    tried forcing homebrewgcc x6

--- Formula/cmod.rb
+++ Formula/cmod.rb
@@ -4,32 +4,25 @@ class Cmod < Formula
   url "https://github.com/AntohiRobert/cmod/archive/refs/heads/main.tar.gz"
   version "0.1.0"
 
-
   depends_on "python@3.12"
-
   depends_on "git"
-
   depends_on "gcc@11"
 
-def install
-  gcc = Formula["gcc@11"]
+  def install
+    gcc = Formula["gcc@11"]
 
-  # 1. Put GCC first in PATH (VERY important)
-  ENV.prepend_path "PATH", gcc.opt_bin
+    ENV.prepend_path "PATH", gcc.opt_bin
 
-  # 3. Extra safety: explicit env vars for build systems
-  ENV["CC"] = gcc.opt_bin/"gcc-11"
-  ENV["CXX"] = gcc.opt_bin/"g++-11"
+    ENV["CC"] = gcc.opt_bin/"gcc-11"
+    ENV["CXX"] = gcc.opt_bin/"g++-11"
 
-  # 4. Prevent accidental clang fallback in some build systems
-  ENV["HOMEBREW_CC"] = gcc.opt_bin/"gcc-11"
-  ENV["HOMEBREW_CXX"] = gcc.opt_bin/"g++-11"
-
-  bin.install "cmod.py" => "cmod"
-end
+    bin.install "cmod.py" => "cmod"
+  end
 
   test do
     system "#{bin}/cmod", "init"
-    system "#{ENV["CXX"]}", "--version"
+
+    gcc = Formula["gcc@11"]
+    system gcc.opt_bin/"g++-11", "--version"
   end
 end
