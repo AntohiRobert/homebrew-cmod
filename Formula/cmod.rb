2a051e876e46fff972641e8a1f689c66841820e0
Antohi Robert <antohir@gmail.com>
2026-04-04 14:43:34 +0300

    changed formula to head dev

--- Formula/cmod.rb
+++ Formula/cmod.rb
@@ -1,26 +1,21 @@
 class Cmod < Formula
   desc "Simple C/C++ modular build system (GCC 12 enforced)"
   homepage "https://github.com/AntohiRobert/cmod"
-  url "https://github.com/AntohiRobert/cmod/archive/refs/heads/main.tar.gz"
-  version "0.1.2"
-  # sha256 "440a1f2817a44218d907a0780bfce500dfee138b46fcaf4dc6fba7e5e59ede0d"
+
+  head "https://github.com/AntohiRobert/cmod.git"
 
   depends_on "python@3.12"
   depends_on "gcc@12"
   depends_on "git"
 
   def install
-    python_bin = Formula["python@3.12"].opt_bin/"python3.12"
-
     libexec.install "cmod.py"
+
+    python = Formula["python@3.12"].opt_bin/"python3.12"
 
     (bin/"cmod").write <<~EOS
       #!/bin/bash
-
-      # Minimal launcher:
-      # GCC 12 selection and sysroot handling are done inside cmod.py
-
-      exec "#{python_bin}" "#{libexec}/cmod.py" "$@"
+      exec "#{python}" "#{libexec}/cmod.py" "$@"
     EOS
 
     chmod 0755, bin/"cmod"
@@ -28,6 +23,5 @@ class Cmod < Formula
 
   test do
     system "#{bin}/cmod", "init"
-    assert_predicate testpath/"cmodconfig.json", :exist?
   end
 end
